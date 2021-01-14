217a3d3ce6831e056089ccd9ae119508dfc8d2a9
Luk Krzy <lukaskrzy@gmail.com>
2021-01-14 18:50:46 +0100

    Add user account page

--- askmate/__init__.py
+++ askmate/__init__.py
@@ -8,6 +8,8 @@ app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager= LoginManager(app)
+login_manager.login_view = 'route_login'
+login_manager.login_message_category = 'info'
 
 from askmate import routes
 from askmate import static
--- askmate/routes.py
+++ askmate/routes.py
@@ -2,56 +2,65 @@ from flask import render_template, url_for, flash, redirect, request
 from askmate import app, db, bcrypt
 from askmate.forms import RegistrationForm, LoginForm
 from askmate.models import Users
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
 @app.route("/home")
 def route_home():
     # print(app.config)
     # print(os.environ)
-    return render_template('home.html')
+    result = db.session.query(Users).all()
+
+    return render_template('home.html', users=result)
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def route_register():
+    form = RegistrationForm()
+
     if current_user.is_authenticated:
         return redirect(url_for('route_home'))
 
-    form = RegistrationForm()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = Users(user_name=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        print(user)
 
         flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('route_home'))
+        return redirect(url_for('route_login'))
     return render_template('register.html', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def route_login():
-    if current_user.is_authenticated:
-        return redirect(url_for('route_home'))
-
     form = LoginForm()
-    if form.validate_on_submit():
+
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('route_home'))
+        return render_template('login.html', form=form)
+
+    elif request.method == 'POST' and form.validate_on_submit():
         # user = db.session.query(Users).filter(Users.email==form.email.data).first()
         user = Users.query.filter_by(email=form.email.data).first()
-        print(user.user_id)
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('route_home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-        return redirect(url_for('route_home'))
-    else:
-        flash('Login Unsuccessful. Please check username and password', 'danger')
+            return redirect(url_for('route_login'))
+
     return render_template('login.html', form=form)
 
 @app.route("/logout")
 def route_logout():
     logout_user()
     return redirect(url_for('route_home'))
+
+
+@app.route("/account")
+@login_required
+def route_account():
+    return render_template('account.html')
